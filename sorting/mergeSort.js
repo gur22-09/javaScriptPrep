998441622d0936a6a630a053ed4c0c1be676f345
GURPRIT SINGH <gurprit.business94@gmail.com>
2020-10-16 18:25:55 +0530

    merge sort added

---
+++ sorting/mergeSort.js
@@ -0,0 +1,47 @@
+/* 
+the idea behind this sorting algorithm is that we keep sorting an n-element array or list into two subarrays of roughtly equal size, then sort the subarrays recursively. Once sorted
+we merge the arrays into a sorted n-element array.
+*/
+
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 100, 4, 0];
+
+function mergeSort (array) {
+  if (array.length === 1) {
+    return array
+  }
+  // Split Array in into right and left
+  const length = array.length;
+  const middle = Math.floor(length / 2)
+  const left = array.slice(0, middle) 
+  const right = array.slice(middle)
+  // console.log('left:', left);
+  // console.log('right:', right);
+
+  
+  return merge(
+    mergeSort(left),
+    mergeSort(right)
+  )
+}
+
+function merge(left, right){
+  const result = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+  while(leftIndex < left.length && 
+        rightIndex < right.length){
+     if(left[leftIndex] < right[rightIndex]){
+       result.push(left[leftIndex]);
+       leftIndex++;
+     } else{
+       result.push(right[rightIndex]);
+       rightIndex++
+    }
+  }  
+  // console.log(left, right)
+  return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+}
+
+
+const answer = mergeSort(numbers);
+console.log(answer);
